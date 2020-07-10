f53ff23815caaad130e3057d7f52f66371b6ef11
Adam Brauer <adab@sitecore.net>
2020-07-10 10:02:01 -0500

    Get-SitecoreRandomString enhancements to allow complexity enforcement. Old "-AlphanumericOnly" flag is replaced by use of new "-DisallowSpecial".

--- powershell/src/Public/Get-SitecoreRandomString.ps1
+++ powershell/src/Public/Get-SitecoreRandomString.ps1
@@ -2,13 +2,31 @@ Set-StrictMode -Version Latest
 
 <#
 .SYNOPSIS
-    Generate a random string e.g. to use as a key or password.
+    Generates a random string of the specified length e.g. to use as a key or password.
+.DESCRIPTION
+    Generates a random string of the specified length using one or more of the 4 allowed types (all by default).
+    The allowed types include: capital letters, lowercase letters, numbers, and the ASCII special printable characters.
+    The -EnforceComplexity option will ensure that at least one of each of the allowed types are present in the string.
 .PARAMETER Length
     The desired length of the string.
-.PARAMETER AlphanumericOnly
-    Limit to use of alphanumeric characters only.
+.PARAMETER EnforceComplexity
+    Ensures the returned string contains at least one of each of the allowed character types.
+.PARAMETER DisallowSpecial
+    Prevent the special characters ~!@#$%^&*_-+=`|(){}[]:;<>.?/
+.PARAMETER DisallowCaps
+    Prevent capital letters from appearing in the generated sting.
+.PARAMETER DisallowLower
+    Prevent lower case letters from appearing in the generated string.
+.PARAMETER DisallowNumbers
+    Prevent numbers from appearing in the generated string.
+.INPUTS
+    None. You cannot pipe objects to Get-SitecoreRandomString.
 .OUTPUTS
     System.String. The random string.
+.EXAMPLE
+    PS C:\> Get-SitecoreRandomString -Length 10
+.EXAMPLE
+    PS C:\> Get-SitecoreRandomString -Length 10 -EnforceComplexity -DisallowSpecial
 #>
 function Get-SitecoreRandomString
 {
@@ -18,30 +36,121 @@ function Get-SitecoreRandomString
         [int]
         $Length,
 
+        [Parameter(Position = 1)]
         [switch]
-        $AlphanumericOnly
+        $EnforceComplexity,
+
+        [Parameter(ParameterSetName = 'custom', Position = 2)]
+        [switch]
+        $DisallowSpecial,
+
+        [Parameter(ParameterSetName = 'custom', Position = 3)]
+        [switch]
+        $DisallowCaps,
+
+        [Parameter(ParameterSetName = 'custom', Position = 4)]
+        [switch]
+        $DisallowLower,
+
+        [Parameter(ParameterSetName = 'custom', Position = 5)]
+        [switch]
+        $DisallowNumbers
     )
 
-    $charset = @(
-        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',`
-        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',`
-        '1','2','3','4','5','6','7','8','9','0'
-    )
+    $complexity = 0
+    $charset = @()
 
-    if (!$AlphanumericOnly) {
+    if ($PSCmdlet.ParameterSetName -ne 'custom') {
+        $DisallowCaps = $false
+        $DisallowLower = $false
+        $DisallowNumbers = $false
+        $DisallowSpecial = $false
+    }
+
+    if (!$DisallowCaps) {
+        $charset = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+        $complexity = $complexity + 1
+    }
+
+    if (!$DisallowLower) {
+        $charset += ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+        $complexity = $complexity + 1
+    }
+
+    if (!$DisallowNumbers) {
+        $charset += ('1','2','3','4','5','6','7','8','9','0')
+        $complexity = $complexity + 1
+    }
+
+    if (!$DisallowSpecial) {
         $charset += ('~','!','@','#','$','%','^','&','*','_','-','+','=','`','|','\','(',')','{','}','[',']',':',';','<','>','.','?','/')
+        $complexity = $complexity + 1
     }
 
-    Write-Verbose "Generating a string of length $Length"
+    if ($EnforceComplexity -and $Length -lt $complexity) {
+        throw "Requested charater types require a minimum length of $complexity characters."
+    }
+
+    if ($complexity -eq 0) {
+        throw "No allowed character types."
+    }
+
     Write-Verbose "Choosing from: $charset"
+    Write-Verbose "Complexity Level: $complexity"
 
-    $string = ""
+    do {
+        Write-Verbose "Generating a string of length $Length"
 
-    for ($i=1; $i -le $Length; $i++) {
-        $string += Get-Random $charset
-    }
+        $generatedString = ""
 
-    Write-Verbose "Generated string is $string"
+        for ($i=1; $i -le $Length; $i++){
+            $generatedString += Get-Random $charset
+        }
 
-    return $string
+        Write-Verbose "Generated string is $generatedString"
+
+        $nums = 0
+        $caps = 0
+        $lower = 0
+        $special = 0
+
+        if ($EnforceComplexity) {
+
+            Write-Verbose "Checking for complexity..."
+
+            $charArray = $generatedString.ToCharArray()
+
+            foreach ($character in $charArray){
+                if ([byte]$character -ge 33 -and [byte]$character -le 47) {
+                    $special = 1
+                }
+                if ([byte]$character -ge 48 -and [byte]$character -le 57) {
+                    $nums = 1
+                }
+                if ([byte]$character -ge 58 -and [byte]$character -le 64) {
+                    $special = 1
+                }
+                if ([byte]$character -ge 65 -and [byte]$character -le 90) {
+                    $caps = 1
+                }
+                if ([byte]$character -ge 91 -and [byte]$character -le 66) {
+                    $special = 1
+                }
+                if ([byte]$character -ge 97 -and [byte]$character -le 122) {
+                    $lower = 1
+                }
+                if ([byte]$character -ge 123 -and [byte]$character -le 126) {
+                    $special = 1
+                }
+            }
+
+            Write-Verbose "Complexity found: $($nums + $caps + $lower + $special)"
+
+        } else {
+           $complexity = $nums + $caps + $lower + $special
+        }
+
+    } while ($nums + $caps + $lower + $special -ne $complexity)
+
+    return $generatedString
 }
--- powershell/test/Public/Get-SitecoreRandomString.Tests.ps1
+++ powershell/test/Public/Get-SitecoreRandomString.Tests.ps1
@@ -21,7 +21,7 @@
         It 'returns requested length' {
             $random = Get-Random -Minimum 8 -Maximum 128
             $key = Get-SitecoreRandomString -Length $random
-            $key.length | should be $random
+            $key.length | Should -Be $random
         }
 
         It 'does not include whitespace in string' {
@@ -29,14 +29,90 @@
             $key | Should -Not -Match " "
         }
 
-        It 'includes non-alphanumeric characters in string by default' {
-            $key = Get-SitecoreRandomString 100
-            $key | Should -Match "[^a-zA-Z0-9]"
+        It 'throws if Length is less than 4 (<Length>) and EnforceComplexty requested' -TestCases @(
+            @{ Length = 1},
+            @{ Length = 3}
+        ){
+            param($Length)
+            { Get-SitecoreRandomString -Length $Length -EnforceComplexity } | Should -Throw
         }
 
-        It 'excludes non-alphanumeric characters in string when requested' {
-            $key = Get-SitecoreRandomString 100 -AlphanumericOnly
-            $key | Should -Not -Match "[^a-zA-Z0-9]"
+        It 'throws if there are no allowed character types' {
+            $random = Get-Random -Minimum 10 -Maximum 20
+
+            { Get-SitecoreRandomString -Length $random -DisallowCaps -DisallowLower -DisallowNumbers -DisallowSpecial } | Should -Throw
+        }
+
+        It 'includes all character types in string by default' {
+            $result = Get-SitecoreRandomString 100
+            $result | Should -MatchExactly "[A-Z]"
+            $result | Should -MatchExactly "[a-z]"
+            $result | Should -MatchExactly "[0-9]"
+            $result | Should -MatchExactly "[^a-zA-Z0-9]"
+        }
+
+        It 'excludes requested character type: Caps' {
+            $random = Get-Random -Minimum 10 -Maximum 20
+            $result = Get-SitecoreRandomString -Length $random -DisallowCaps
+            $result | Should -Not -MatchExactly "[A-Z]"
+        }
+
+        It 'excludes requested character type: Lowercase' {
+            $random = Get-Random -Minimum 10 -Maximum 20
+            $result = Get-SitecoreRandomString -Length $random -DisallowLower
+            $result | Should -Not -MatchExactly "[a-z]"
+        }
+
+        It 'excludes requested character type: Numbers' {
+            $random = Get-Random -Minimum 10 -Maximum 20
+            $result = Get-SitecoreRandomString -Length $random -DisallowNumbers
+            $result | Should -Not -MatchExactly "[0-9]"
+        }
+
+        It 'excludes requested character type: Special' {
+            $random = Get-Random -Minimum 10 -Maximum 20
+            $result = Get-SitecoreRandomString -Length $random -DisallowSpecial
+            $result | Should -Not -MatchExactly "[^a-zA-Z0-9]"
+        }
+
+        It 'meets complexity requirements' {
+            $random = Get-Random -Minimum 15 -Maximum 25
+            $result = Get-SitecoreRandomString -Length $random -EnforceComplexity
+
+            $nums = 0
+            $caps = 0
+            $lower = 0
+            $special = 0
+
+            $charArray = $result.ToCharArray()
+
+            foreach ($character in $charArray) {
+
+                if ([byte]$character -ge 33 -and [byte]$character -le 47) {
+                    $special = 1
+                }
+                if ([byte]$character -ge 48 -and [byte]$character -le 57) {
+                    $nums = 1
+                }
+                if ([byte]$character -ge 58 -and [byte]$character -le 64) {
+                    $special = 1
+                }
+                if ([byte]$character -ge 65 -and [byte]$character -le 90) {
+                    $caps = 1
+                }
+                if ([byte]$character -ge 91 -and [byte]$character -le 96) {
+                    $special = 1
+                }
+                if ([byte]$character -ge 97 -and [byte]$character -le 122) {
+                    $lower = 1
+                }
+                if ([byte]$character -ge 123 -and [byte]$character -le 126) {
+                    $special = 1
+                }
+            }
+            $total = $nums + $caps + $lower + $special
+
+            $total | Should -eq 4
         }
     }
 }
