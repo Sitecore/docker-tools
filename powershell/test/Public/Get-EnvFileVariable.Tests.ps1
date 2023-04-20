cd424f2bc0916bcc06f25febcac1a8fcc11a7b96
Peter N <pnz@hotmail.co.uk>
2023-04-20 10:55:53 +0200

    Get-EnvFileContent fix (#40)
    * Added additional Unit Tests to identify a bug on "Get-EnvFileVariable"
    
    * Fixed Unit Tests identified bugs: Removed redundant validator on Get-EnvFileContent function's Path parameter
    
    ---------
    
    Co-authored-by: peter.nazarov <peter.nazarov@valtech.com>

--- powershell/test/Public/Get-EnvFileVariable.Tests.ps1
+++ powershell/test/Public/Get-EnvFileVariable.Tests.ps1
@@ -1,15 +1,23 @@
 . $PSScriptRoot\..\TestRunner.ps1 {
     . $PSScriptRoot\..\TestUtils.ps1
 
-    Describe 'Get-EnvFileVariable' {
+    Describe 'Get-EnvFileVariable' {       
 
-        $envFile = Join-Path $TestDrive '.env'
+        BeforeEach {
+            Push-Location $TestDrive
+        }
+
+        AfterEach {
+            Pop-Location
+        }
+            
+        $envFile = '.env'
         $content = @(
             'VAR1=VAL1',
             'VAR2=VAL2',
             'VAR3=VAL3'
         )
-        Set-Content $envFile -Value $content
+        Set-Content "$TestDrive\$envFile" -Value $content
 
         It 'requires $Variable' {
             $result = Test-ParamIsMandatory -Command Get-EnvFileVariable -Parameter Variable
@@ -17,7 +25,7 @@
         }
 
         It 'throws if $Path is invalid' {
-            { Get-EnvFileVariable -Variable "foo" -Value "bar" -Path "$TestDrive\.baz" } | Should -Throw
+            { Get-EnvFileVariable -Variable "foo" -Value "bar" -Path "$.baz" } | Should -Throw
         }
 
         It 'throws if $Variable is $null or empty' {
@@ -29,9 +37,21 @@
             { Get-EnvFileVariable -Path $envFile -Variable 'VAR' } | Should -Throw
         }
 
-        It 'reads variable correctly' {
+        It 'reads variable correctly usiing default file path' {
+            $value = 'VAL2'
+            $result = Get-EnvFileVariable -Variable 'VAR2'
+            $result | Should -Be $value
+        }
+
+        It 'reads variable correctly usiing relative file path' {
             $value = 'VAL2'
             $result = Get-EnvFileVariable -Path $envFile -Variable 'VAR2'
+            $result | Should -Be $value
+        }
+
+        It 'reads variable correctly usiing absolute file path' {
+            $value = 'VAL2'
+            $result = Get-EnvFileVariable -Path "$TestDrive\$envFile" -Variable 'VAR2'
             $result | Should -Be $value
         }
     }
